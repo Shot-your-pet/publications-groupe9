c32dc91454e835d61a1272ee532f71e41e6bcfd2
Aymeric SERRA <oupson1er@gmail.com>
2025-03-07 09:40:10 +0100

    feat: Initialize repository.

---
+++ src/main/kotlin/fr/miage/syp/publication/PublicationApplication.kt
@@ -0,0 +1,11 @@
+package fr.miage.syp.publication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PublicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<PublicationApplication>(*args)
+}
---
+++ src/test/kotlin/fr/miage/syp/publication/PublicationApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.miage.syp.publication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PublicationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
