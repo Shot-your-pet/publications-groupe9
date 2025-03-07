5ec646e20670d1c7fe5f836f895031ca9a84b0ee
Aymeric SERRA <oupson1er@gmail.com>
2025-03-07 12:07:08 +0100

    feat: Add security config for new publication route and error dispatcher.

--- src/main/kotlin/fr/miage/syp/publication/config/PublicationConfig.kt
+++ src/main/kotlin/fr/miage/syp/publication/config/PublicationConfig.kt
@@ -1,24 +1,26 @@
 package fr.miage.syp.publication.config
 
+import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
-@EnableWebSecurity
 class PublicationConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http.csrf { it.disable() }.authorizeHttpRequests { auth ->
-                auth.anyRequest().denyAll()
-            }.oauth2ResourceServer { oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()) }
+            auth.requestMatchers(HttpMethod.OPTIONS, "/posts/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/posts/").authenticated()
+                .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll() // todo
+                .anyRequest().denyAll()
+        }.oauth2ResourceServer { oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()) }
             .cors(Customizer.withDefaults()).build()
 
 
