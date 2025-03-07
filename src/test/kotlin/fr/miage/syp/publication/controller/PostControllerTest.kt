73411f716f1d4cd7bbbb2a86a2536966ee757da9
Aymeric SERRA <oupson1er@gmail.com>
2025-03-07 12:07:28 +0100

    feat: Add test for new publication route.

---
+++ src/test/kotlin/fr/miage/syp/publication/controller/PostControllerTest.kt
@@ -0,0 +1,65 @@
+package fr.miage.syp.publication.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import fr.miage.syp.publication.model.NewPost
+import fr.miage.syp.publication.service.PostService
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.doReturn
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import java.util.*
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostControllerTest {
+    companion object {
+        private const val USER_UUID = "cd4b40e4-f532-4acc-812e-9d5ed8ac1267"
+    }
+
+    @Autowired
+    lateinit var mvc: MockMvc
+
+    private val mapper = ObjectMapper()
+
+    @MockitoBean
+    lateinit var postService: PostService
+
+    private val random = Random()
+
+    @Test
+    fun `test create post without authentication is unauthorized`() {
+        mvc.perform(MockMvcRequestBuilders.post("/posts/")).andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    }
+
+    @Test
+    @WithMockUser(username = USER_UUID)
+    fun `test create post with authentication no content create object`() {
+        val newId = random.nextInt()
+        doReturn(newId).`when`(postService).createDraftedPostForUser(UUID.fromString(USER_UUID), null)
+        val content = mapper.writeValueAsString(NewPost(null))
+        mvc.perform(
+            MockMvcRequestBuilders.post("/posts/").contentType(MediaType.APPLICATION_JSON).content(content)
+        ).andExpect(MockMvcResultMatchers.status().isCreated).andExpect(jsonPath("$.id").value(newId))
+    }
+
+    @Test
+    @WithMockUser(username = USER_UUID)
+    fun `test create post with authentication with content create object`() {
+        val newId = random.nextInt()
+        val createPostContent = "bar"
+        doReturn(newId).`when`(postService).createDraftedPostForUser(UUID.fromString(USER_UUID), createPostContent)
+        val content = mapper.writeValueAsString(NewPost(createPostContent))
+        mvc.perform(
+            MockMvcRequestBuilders.post("/posts/").contentType(MediaType.APPLICATION_JSON).content(content)
+        ).andExpect(MockMvcResultMatchers.status().isCreated).andExpect(jsonPath("$.id").value(newId))
+    }
+}
