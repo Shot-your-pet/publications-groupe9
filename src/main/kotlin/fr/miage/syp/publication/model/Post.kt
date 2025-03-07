f75f77dacc74f25dafe73acf60a3251bfff3269d
Aymeric SERRA <oupson1er@gmail.com>
2025-03-07 17:42:33 +0100

    feat: Add model Post.

---
+++ src/main/kotlin/fr/miage/syp/publication/model/Post.kt
@@ -0,0 +1,13 @@
+package fr.miage.syp.publication.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.Instant
+import java.util.*
+
+data class Post(
+    val id: Long,
+    @JsonProperty("author_id") val authorId: UUID,
+    val content: String?,
+    @JsonProperty("published_at") val publishedAt: Instant,
+    @JsonProperty("image_id") val imageId: Long,
+)
