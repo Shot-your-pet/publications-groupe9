fbe0b720ea13449b3f12c2cf464c401515c29742
Aymeric SERRA <oupson1er@gmail.com>
2025-03-07 17:18:36 +0100

    feat: Add method to find post not drafted in PostRepository.

--- src/main/kotlin/fr/miage/syp/publication/data/repository/PostRepository.kt
+++ src/main/kotlin/fr/miage/syp/publication/data/repository/PostRepository.kt
@@ -1,8 +1,11 @@
 package fr.miage.syp.publication.data.repository
 
 import fr.miage.syp.publication.data.model.Post
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PostRepository : MongoRepository<Post, Long>
+interface PostRepository : MongoRepository<Post, Long> {
+    fun findPostByImageIdNotNull(pageable: Pageable): List<Post>
+}
