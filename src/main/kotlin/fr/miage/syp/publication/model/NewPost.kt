2d6be1984171158536341ba20625e1f2197dd260
Aymeric SERRA <oupson1er@gmail.com>
2025-03-08 20:35:20 +0100

    feat: Take challenge id in new post and save it in document.

--- src/main/kotlin/fr/miage/syp/publication/model/NewPost.kt
+++ src/main/kotlin/fr/miage/syp/publication/model/NewPost.kt
@@ -1,5 +1,8 @@
 package fr.miage.syp.publication.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class NewPost(
     val content: String?,
+    @JsonProperty("challenge_id") val challengeId: Long,
 )
