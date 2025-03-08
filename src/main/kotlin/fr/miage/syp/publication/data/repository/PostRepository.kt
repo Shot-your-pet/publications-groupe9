995079e44250ca5d376ec9b767cec62df2b7726b
Aymeric SERRA <oupson1er@gmail.com>
2025-03-08 22:09:15 +0100

    feat: Add method to test if a user have already submitted a post for a given challenge.

--- src/main/kotlin/fr/miage/syp/publication/data/repository/PostRepository.kt
+++ src/main/kotlin/fr/miage/syp/publication/data/repository/PostRepository.kt
@@ -4,8 +4,10 @@ import fr.miage.syp.publication.data.model.Post
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface PostRepository : MongoRepository<Post, Long> {
+    fun existsPostByAuthorIdAndChallengeId(authorId: UUID, challengeId: Long): Boolean
     fun findPostByImageIdNotNull(pageable: Pageable): List<Post>
 }
