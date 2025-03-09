1d735af59ff061fcf88947b8d0ee2ae6ea2d5748
Aymeric SERRA <oupson1er@gmail.com>
2025-03-09 11:45:42 +0100

    feat: Add Drafted post and PublishedPost as type.

--- src/main/kotlin/fr/miage/syp/publication/model/Post.kt
+++ src/main/kotlin/fr/miage/syp/publication/model/Post.kt
@@ -1,14 +1,42 @@
 package fr.miage.syp.publication.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 import java.time.Instant
 import java.util.*
 
-data class Post(
-    val id: Long,
-    @JsonProperty("author_id") val authorId: UUID,
-    @JsonProperty("challenge_id") val challengeId: Long,
-    val content: String?,
-    @JsonProperty("published_at") val publishedAt: Instant,
-    @JsonProperty("image_id") val imageId: Long,
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "type"
 )
+@JsonSubTypes(
+    JsonSubTypes.Type(DraftedPost::class, name = "drafted"),
+    JsonSubTypes.Type(Post.PublishedPost::class, name = "published")
+)
+sealed interface Post {
+    val id: Long
+    val authorId: UUID
+    val challengeId: Long
+    val content: String?
+    val publishedAt: Instant?
+
+    @JsonTypeName("drafted")
+    data class DraftedPost(
+        override val id: Long,
+        @JsonProperty("author_id") override val authorId: UUID,
+        @JsonProperty("challenge_id") override val challengeId: Long,
+        override val content: String?,
+        @JsonProperty("published_at") override val publishedAt: Instant,
+    ) : Post
+
+    @JsonTypeName("published")
+    data class PublishedPost(
+        override val id: Long,
+        @JsonProperty("author_id") override val authorId: UUID,
+        @JsonProperty("challenge_id") override val challengeId: Long,
+        override val content: String?,
+        @JsonProperty("published_at") override val publishedAt: Instant,
+        @JsonProperty("image_id") val imageId: Long,
+    ) : Post
+}
