3c05b7410c2a371c5368251550ac33e533f10c6f
Aymeric SERRA <oupson1er@gmail.com>
2025-03-10 15:55:22 +0100

    fix: Remove useless DraftedPost DTO.

--- src/test/kotlin/fr/miage/syp/publication/controller/PublishedPostControllerTest.kt
+++ src/test/kotlin/fr/miage/syp/publication/controller/PublishedPostControllerTest.kt
@@ -3,7 +3,6 @@ package fr.miage.syp.publication.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import fr.miage.syp.publication.data.exception.ChallengeAlreadyCompletedException
-import fr.miage.syp.publication.model.DraftedPost
 import fr.miage.syp.publication.model.NewPost
 import fr.miage.syp.publication.model.Post
 import fr.miage.syp.publication.service.MessagingService
@@ -48,20 +47,6 @@ class PublishedPostControllerTest {
     @Test
     fun `getPost without authentication is unauthorized`() {
         mvc.perform(MockMvcRequestBuilders.post("/posts/${random.nextLong()}")).andExpect(status().isUnauthorized)
-    }
-
-    @Test
-    @WithMockUser(username = USER_UUID)
-    fun `getPost should return 200 OK when drafted post is found`() {
-        val postId = random.nextLong()
-        val authorId = UUID.randomUUID()
-        val challengeId = random.nextLong()
-        val post = Post.DraftedPost(postId, authorId, challengeId, "Content", Instant.now())
-        doReturn(post).`when`(postService).getPost(postId)
-        mvc.perform(
-            get("/posts/$postId").contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(status().isOk).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.drafted.id").value(postId)).andExpect(jsonPath("$.drafted.content").value("Content"))
     }
 
     @Test
@@ -150,7 +135,7 @@ class PublishedPostControllerTest {
         val createPostContent = "bar"
         val challengeId = random.nextLong()
         val imageId = random.nextLong()
-        doReturn(Result.failure<DraftedPost>(ChallengeAlreadyCompletedException())).`when`(postService)
+        doReturn(Result.failure<Post.PublishedPost>(ChallengeAlreadyCompletedException())).`when`(postService)
             .createPostForUser(UUID.fromString(USER_UUID), challengeId, createPostContent, imageId)
         val content = mapper.writeValueAsString(NewPost(createPostContent, challengeId, imageId))
         mvc.perform(
--- src/test/kotlin/fr/miage/syp/publication/service/PublishedPostServiceTest.kt
+++ src/test/kotlin/fr/miage/syp/publication/service/PublishedPostServiceTest.kt
@@ -1,14 +1,11 @@
 package fr.miage.syp.publication.service
 
 import fr.miage.syp.publication.data.exception.ChallengeAlreadyCompletedException
-import fr.miage.syp.publication.data.exception.PostAlreadyPublishedException
-import fr.miage.syp.publication.data.exception.PostNotFoundException
 import fr.miage.syp.publication.data.model.Post
 import fr.miage.syp.publication.data.repository.PostRepository
 import fr.miage.syp.publication.services.SnowflakeIdGenerator
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.mockito.ArgumentMatchers.anyLong
 import org.mockito.Mockito.*
 import org.springframework.beans.factory.annotation.Autowired
@@ -39,7 +36,7 @@ class PublishedPostServiceTest {
     private val random = SecureRandom()
 
     @Test
-    fun `getPost should return PublishedPost when imageId is not null`() {
+    fun `getPost should return PublishedPost when post is found`() {
         val postId = random.nextLong()
         val post = Post(
             id = postId,
@@ -54,28 +51,9 @@ class PublishedPostServiceTest {
         val result = postService.getPost(postId)
         assertEquals(
             ModelPost.PublishedPost(
-                postId, post.authorId, post.challengeId, post.content, post.publishedAt, post.imageId!!
+                postId, post.authorId, post.challengeId, post.content, post.publishedAt, post.imageId
             ),
             result,
-        )
-    }
-
-    @Test
-    fun `getPost should return DraftedPost when imageId is null`() {
-        val postId = random.nextLong()
-        val post = Post(
-            id = postId,
-            authorId = UUID.randomUUID(),
-            challengeId = random.nextLong(),
-            content = "Content",
-            publishedAt = Instant.now(),
-            imageId = null,
-            likedBy = emptyList()
-        )
-        doReturn(Optional.of(post)).`when`(postRepository).findById(postId)
-        val result = postService.getPost(postId)
-        assertEquals(
-            ModelPost.DraftedPost(postId, post.authorId, post.challengeId, post.content, post.publishedAt), result
         )
     }
 
@@ -124,38 +102,6 @@ class PublishedPostServiceTest {
         assertEquals("content", result[0].content)
         assertEquals(postEntity.publishedAt, result[0].publishedAt)
         assertEquals(imageId, result[0].imageId)
-    }
-
-    @Test
-    fun `should throw exception when imageId is null`() {
-        // Arrange
-        val page = 0
-        val maxSize = 10
-        val authorId = UUID.randomUUID()
-        val postEntity = Post(
-            id = 1,
-            authorId = authorId,
-            challengeId = 0L,
-            content = "content",
-            publishedAt = Instant.now(),
-            imageId = null,
-            likedBy = emptyList()
-        )
-        val postEntityList = listOf(postEntity)
-
-        `when`(
-            postRepository.findPostByImageIdNotNull(
-                PageRequest.of(
-                    page, maxSize, Sort.by(Sort.Direction.DESC, "id")
-                )
-            )
-        ).thenReturn(postEntityList)
-
-        // Act & Assert
-        val exception = assertThrows<IllegalArgumentException> {
-            postService.getPosts(page, maxSize)
-        }
-        assertEquals("imageId cannot be null", exception.message)
     }
 
     @Test
@@ -265,7 +211,7 @@ class PublishedPostServiceTest {
             val challengeId = Random().nextLong()
             val imageId = random.nextLong()
             val createdPost = Post(
-                newId, uuid, challengeId, "foo", now, null, emptyList()
+                newId, uuid, challengeId, "foo", now, imageId, emptyList()
             )
 
             doReturn(true).`when`(postRepository).existsPostByAuthorIdAndChallengeId(uuid, challengeId)
@@ -282,69 +228,6 @@ class PublishedPostServiceTest {
         doNothing().`when`(postRepository).deleteById(id)
         postService.removePost(id)
         verify(postRepository, times(1)).deleteById(id)
-    }
-
-    @Test
-    fun `setImageIdForPost should return failure when post is not found`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        doReturn(Optional.empty<Post>()).`when`(postRepository).findById(postId)
-        val result = postService.setImageIdForPost(postId, imageId)
-        assertTrue(result.isFailure)
-        assertEquals(PostNotFoundException::class, result.exceptionOrNull()!!::class)
-    }
-
-    @Test
-    fun `setImageIdForPost should return failure when post is already published`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        val authorId = UUID.randomUUID()
-        val post = Post(
-            id = postId,
-            authorId = authorId,
-            challengeId = random.nextLong(),
-            content = "Content",
-            publishedAt = Instant.now(),
-            imageId = imageId,
-            likedBy = emptyList()
-        )
-        doReturn(Optional.of(post)).`when`(postRepository).findById(postId)
-        val result = postService.setImageIdForPost(postId, imageId)
-        assertTrue(result.isFailure)
-        assertEquals(PostAlreadyPublishedException::class, result.exceptionOrNull()!!::class)
-    }
-
-    @Test
-    fun `setImageIdForPost should return success when imageId is set successfully`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        val authorId = UUID.randomUUID()
-        val post = Post(
-            id = postId,
-            authorId = authorId,
-            challengeId = random.nextLong(),
-            content = "Content",
-            publishedAt = Instant.now(),
-            imageId = null,
-            likedBy = emptyList()
-        )
-        doReturn(Optional.of(post)).`when`(postRepository).findById(postId)
-        val newPost = post.copy(imageId = imageId)
-        doReturn(newPost).`when`(postRepository).save(newPost)
-
-        val result = postService.setImageIdForPost(postId, imageId)
-        assertTrue(result.isSuccess)
-        assertEquals(
-            ModelPost.PublishedPost(
-                newPost.id,
-                newPost.authorId,
-                newPost.challengeId,
-                newPost.content,
-                newPost.publishedAt,
-                newPost.imageId!!
-            ), result.getOrNull()
-        )
-        verify(postRepository).save(post.copy(imageId = imageId))
     }
 
     fun withMockedInstant(block: (now: Instant) -> Unit) {
