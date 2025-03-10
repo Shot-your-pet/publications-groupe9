43002ded8e23d792c10118681afe178816939905
Aymeric SERRA <oupson1er@gmail.com>
2025-03-10 15:44:35 +0100

    fix: Remove now useless listener in MessagingService.

--- src/main/kotlin/fr/miage/syp/publication/service/MessagingService.kt
+++ src/main/kotlin/fr/miage/syp/publication/service/MessagingService.kt
@@ -2,9 +2,7 @@ package fr.miage.syp.publication.service
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import fr.miage.syp.publication.model.Post
-import org.slf4j.LoggerFactory
 import org.springframework.amqp.AmqpException
-import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -13,17 +11,9 @@ import java.util.*
 
 @Service
 class MessagingService(
-    private val postService: PostService,
     private val rabbitTemplate: RabbitTemplate,
     @Value("\${publish.broadcastExchangeName}") private val broadcastExchangeName: String,
 ) {
-    private val logger = LoggerFactory.getLogger(this::class.java)
-
-    data class PublishedImageMessage(
-        @JsonProperty("post_id") val postId: Long,
-        @JsonProperty("image_id") val imageId: Long,
-    )
-
     data class PostMessage(
         val id: Long,
         @JsonProperty("author_id") val authorId: UUID,
@@ -45,15 +35,6 @@ class MessagingService(
     @Throws(AmqpException::class)
     fun sendPostToBus(post: Post.PublishedPost) {
         rabbitTemplate.convertAndSend(broadcastExchangeName, PUBLISH_ROUTING_KEY, PostMessage(post))
-    }
-
-    @RabbitListener(queues = ["\${publish.imagePublishedQueueName}"])
-    fun onImageUploads(message: PublishedImageMessage) {
-        postService.setImageIdForPost(message.postId, message.imageId).mapCatching { published ->
-            rabbitTemplate.convertAndSend(broadcastExchangeName, PUBLISH_ROUTING_KEY, PostMessage(published))
-        }.onFailure {
-            logger.error("failed to save image for post ${message.postId}", it)
-        }
     }
 
     companion object {
--- src/test/kotlin/fr/miage/syp/publication/service/MessagingServiceTest.kt
+++ src/test/kotlin/fr/miage/syp/publication/service/MessagingServiceTest.kt
@@ -1,7 +1,5 @@
 package fr.miage.syp.publication.service
 
-import fr.miage.syp.publication.data.exception.PostAlreadyPublishedException
-import fr.miage.syp.publication.data.exception.PostNotFoundException
 import fr.miage.syp.publication.model.Post
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -9,7 +7,6 @@ import org.mockito.kotlin.*
 import org.springframework.amqp.AmqpException
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import java.security.SecureRandom
@@ -30,52 +27,6 @@ class MessagingServiceTest {
 
     @MockitoBean
     lateinit var rabbitTemplate: RabbitTemplate
-
-    @Value("\${publish.broadcastExchangeName}")
-    lateinit var broadcastExchangeName: String
-
-    @Test
-    fun `onImageUploads for drafted post is successful`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        val post = Post.PublishedPost(postId, UUID.randomUUID(), random.nextLong(), "foo", Instant.now(), imageId)
-        val messagePost = MessagingService.PostMessage(post)
-        doReturn(Result.success(post)).`when`(postService).setImageIdForPost(postId, imageId)
-        doNothing().`when`(rabbitTemplate).convertAndSend(
-            broadcastExchangeName, MessagingService.PUBLISH_ROUTING_KEY, messagePost
-        )
-        messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
-        verify(postService, times(1)).setImageIdForPost(postId, imageId)
-        verify(rabbitTemplate, times(1)).convertAndSend(
-            broadcastExchangeName, MessagingService.PUBLISH_ROUTING_KEY, messagePost
-        )
-    }
-
-    @Test
-    fun `onImageUploads for published post will fail`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        doReturn(Result.failure<Post.PublishedPost>(PostAlreadyPublishedException())).`when`(postService)
-            .setImageIdForPost(postId, imageId)
-        messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
-        verify(postService, times(1)).setImageIdForPost(postId, imageId)
-        verify(rabbitTemplate, times(0)).convertAndSend(
-            any(), any(), any<Any>()
-        )
-    }
-
-    @Test
-    fun `onImageUploads for not found post will fail`() {
-        val postId = random.nextLong()
-        val imageId = random.nextLong()
-        doReturn(Result.failure<Post.PublishedPost>(PostNotFoundException())).`when`(postService)
-            .setImageIdForPost(postId, imageId)
-        messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
-        verify(postService, times(1)).setImageIdForPost(postId, imageId)
-        verify(rabbitTemplate, times(0)).convertAndSend(
-            any(), any(), any<Any>()
-        )
-    }
 
     @Test
     fun `sendImageToBus call rabbit template with good parameters`() {
