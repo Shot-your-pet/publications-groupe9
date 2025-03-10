ddb1ee5d61c5ad231ff627d4f9a5385a70536e7d
Aymeric SERRA <oupson1er@gmail.com>
2025-03-10 14:55:31 +0100

    feat: Add imageId to NewPost.

--- src/main/kotlin/fr/miage/syp/publication/model/NewPost.kt
+++ src/main/kotlin/fr/miage/syp/publication/model/NewPost.kt
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class NewPost(
     val content: String?,
     @JsonProperty("challenge_id") val challengeId: Long,
+    @JsonProperty("content_type") val imageId: Long,
 )
