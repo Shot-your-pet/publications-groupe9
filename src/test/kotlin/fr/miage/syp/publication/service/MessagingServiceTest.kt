eca71a3f8950e965a29c226aaba8709a1b2bd9ed
Aymeric SERRA <oupson1er@gmail.com>
2025-03-10 11:34:30 +0100

    feat: Add test for publications on the bus in MessageService.

--- src/test/kotlin/fr/miage/syp/publication/service/MessagingServiceTest.kt
+++ src/test/kotlin/fr/miage/syp/publication/service/MessagingServiceTest.kt
@@ -5,7 +5,9 @@ import fr.miage.syp.publication.data.exception.PostNotFoundException
 import fr.miage.syp.publication.model.Post
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import java.security.SecureRandom
@@ -15,7 +17,6 @@ import java.util.*
 
 @SpringBootTest
 class MessagingServiceTest {
-
     @MockitoBean
     lateinit var postService: PostService
 
@@ -24,15 +25,27 @@ class MessagingServiceTest {
     @Autowired
     lateinit var messageService: MessagingService
 
+    @MockitoBean
+    lateinit var rabbitTemplate: RabbitTemplate
+
+    @Value("\${publish.broadcastExchangeName}")
+    lateinit var broadcastExchangeName: String
 
     @Test
     fun `onImageUploads for drafted post is successful`() {
         val postId = random.nextLong()
         val imageId = random.nextLong()
         val post = Post.PublishedPost(postId, UUID.randomUUID(), random.nextLong(), "foo", Instant.now(), imageId)
+        val messagePost = MessagingService.PostMessage(post)
         doReturn(Result.success(post)).`when`(postService).setImageIdForPost(postId, imageId)
+        doNothing().`when`(rabbitTemplate).convertAndSend(
+            broadcastExchangeName, MessagingService.PUBLISH_ROUTING_KEY, messagePost
+        )
         messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
         verify(postService, times(1)).setImageIdForPost(postId, imageId)
+        verify(rabbitTemplate, times(1)).convertAndSend(
+            broadcastExchangeName, MessagingService.PUBLISH_ROUTING_KEY, messagePost
+        )
     }
 
     @Test
@@ -43,6 +56,9 @@ class MessagingServiceTest {
             .setImageIdForPost(postId, imageId)
         messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
         verify(postService, times(1)).setImageIdForPost(postId, imageId)
+        verify(rabbitTemplate, times(0)).convertAndSend(
+            anyString(), anyString(), any<Any>()
+        )
     }
 
     @Test
@@ -53,6 +69,9 @@ class MessagingServiceTest {
             .setImageIdForPost(postId, imageId)
         messageService.onImageUploads(MessagingService.PublishedImageMessage(postId, imageId))
         verify(postService, times(1)).setImageIdForPost(postId, imageId)
+        verify(rabbitTemplate, times(0)).convertAndSend(
+            anyString(), anyString(), any<Any>()
+        )
     }
 }
 
